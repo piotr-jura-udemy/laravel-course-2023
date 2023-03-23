7eb1d7cb62025bdfce5a0b6d800c3c2cb0b245d8
Piotr Jura <jura.piotrek@gmail.com>
2023-03-23 19:31:26 +0100

    0002-all-about-routing

--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -14,5 +14,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Main Page';
 });
+
+Route::get('/xxx', function () {
+    return 'Hello';
+})->name('hello');
+
+Route::get('/hallo', function () {
+    return redirect()->route('hello');
+});
+
+Route::get('/greet/{name}', function ($name) {
+    return 'Hello ' . $name . '!';
+});
+
+Route::fallback(function () {
+    return 'Still got somewhere!';
+});
+
+// GET
+// POST 
+// PUT 
+// DELETE 
