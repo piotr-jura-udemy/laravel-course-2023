4108bd05ecc1e2f66c48734561e61d0e8602c7c0
Piotr Jura <jura.piotrek@gmail.com>
2023-03-26 15:11:38 +0200

    0004-blade-continued

--- l10-task-list/resources/views/index.blade.php
+++ l10-task-list/resources/views/index.blade.php
@@ -1,7 +1,15 @@
-<div>
-  Hello Im a blade template!
-</div>
+<h1>
+  The list of tasks
+</h1>
 
-@isset($name)
-  <div>The name is: {{ $name }}</div>
-@endisset
+<div>
+  {{-- @if (count($tasks)) --}}
+  @forelse ($tasks as $task)
+    <div>
+      <a href="{{ route('tasks.show', ['id' => $task->id]) }}">{{ $task->title }}</a>
+    </div>
+  @empty
+    <div>There are no tasks!</div>
+  @endforelse
+  {{-- @endif --}}
+</div>
--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -13,29 +13,81 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+class Task
+{
+    public function __construct(
+        public int $id,
+        public string $title,
+        public string $description,
+        public ?string $long_description,
+        public bool $completed,
+        public string $created_at,
+        public string $updated_at
+    ) {
+    }
+}
+
+$tasks = [
+    new Task(
+        1,
+        'Buy groceries',
+        'Task 1 description',
+        'Task 1 long description',
+        false,
+        '2023-03-01 12:00:00',
+        '2023-03-01 12:00:00'
+    ),
+    new Task(
+        2,
+        'Sell old stuff',
+        'Task 2 description',
+        null,
+        false,
+        '2023-03-02 12:00:00',
+        '2023-03-02 12:00:00'
+    ),
+    new Task(
+        3,
+        'Learn programming',
+        'Task 3 description',
+        'Task 3 long description',
+        true,
+        '2023-03-03 12:00:00',
+        '2023-03-03 12:00:00'
+    ),
+    new Task(
+        4,
+        'Take dogs for a walk',
+        'Task 4 description',
+        null,
+        false,
+        '2023-03-04 12:00:00',
+        '2023-03-04 12:00:00'
+    ),
+];
+
+Route::get('/', function () use ($tasks) {
     return view('index', [
-        'name' => 'Piotr'
+        'tasks' => $tasks
     ]);
-});
+})->name('tasks.index');
 
-Route::get('/xxx', function () {
-    return 'Hello';
-})->name('hello');
+Route::get('/{id}', function ($id) {
+    return 'One single task';
+})->name('tasks.show');
 
-Route::get('/hallo', function () {
-    return redirect()->route('hello');
-});
+// Route::get('/xxx', function () {
+//     return 'Hello';
+// })->name('hello');
 
-Route::get('/greet/{name}', function ($name) {
-    return 'Hello ' . $name . '!';
-});
+// Route::get('/hallo', function () {
+//     return redirect()->route('hello');
+// });
+
+// Route::get('/greet/{name}', function ($name) {
+//     return 'Hello ' . $name . '!';
+// });
 
 Route::fallback(function () {
     return 'Still got somewhere!';
 });
-
-// GET
-// POST 
-// PUT 
-// DELETE 
