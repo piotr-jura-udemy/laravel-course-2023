d2f3bd2c398cbfe33d7ca3989d16bf0ed3825946
Piotr Jura <jura.piotrek@gmail.com>
2023-03-28 19:42:02 +0200

    0008-factory-seeder

---
+++ l10-task-list/database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence,
+            'description' => fake()->paragraph,
+            'long_description' => fake()->paragraph(7, true),
+            'completed' => fake()->boolean
+        ];
+    }
+}
--- l10-task-list/database/seeders/DatabaseSeeder.php
+++ l10-task-list/database/seeders/DatabaseSeeder.php
@@ -12,7 +12,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(2)->unverified()->create();
+        \App\Models\Task::factory(20)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
