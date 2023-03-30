462205ba2c00b6735129d7e4b03b8c6b0099bd89
Piotr Jura <jura.piotrek@gmail.com>
2023-03-30 14:14:38 +0200

    0013-edit-form

--- l10-task-list/resources/views/edit.blade.php
+++ l10-task-list/resources/views/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Add Task')
+@section('title', 'Edit Task')
 
 @section('styles')
   <style>
@@ -12,13 +12,14 @@
 @endsection
 
 @section('content')
-  <form method="POST" action="{{ route('tasks.store') }}">
+  <form method="POST" action="{{ route('tasks.update', ['id' => $task->id]) }}">
     @csrf
+    @method('PUT')
     <div>
       <label for="title">
         Title
       </label>
-      <input text="text" name="title" id="title" />
+      <input text="text" name="title" id="title" value="{{ $task->title }}" />
       @error('title')
         <p class="error-message">{{ $message }}</p>
       @enderror
@@ -26,7 +27,7 @@
 
     <div>
       <label for="description">Description</label>
-      <textarea name="description" id="description" rows="5"></textarea>
+      <textarea name="description" id="description" rows="5">{{ $task->description }}</textarea>
       @error('description')
         <p class="error-message">{{ $message }}</p>
       @enderror
@@ -34,14 +35,14 @@
 
     <div>
       <label for="long_description">Long Description</label>
-      <textarea name="long_description" id="long_description" rows="10"></textarea>
+      <textarea name="long_description" id="long_description" rows="10">{{ $task->long_description }}</textarea>
       @error('long_description')
         <p class="error-message">{{ $message }}</p>
       @enderror
     </div>
 
     <div>
-      <button type="submit">Add Task</button>
+      <button type="submit">Edit Task</button>
     </div>
   </form>
 @endsection
--- l10-task-list/routes/web.php
+++ l10-task-list/routes/web.php
@@ -29,6 +29,12 @@ Route::get('/tasks', function () {
 Route::view('/tasks/create', 'create')
     ->name('tasks.create');
 
+Route::get('/tasks/{id}/edit', function ($id) {
+    return view('edit', [
+        'task' => Task::findOrFail($id)
+    ]);
+})->name('tasks.edit');
+
 Route::get('/tasks/{id}', function ($id) {
     return view('show', [
         'task' => Task::findOrFail($id)
@@ -51,6 +57,23 @@ Route::post('/tasks', function (Request $request) {
     return redirect()->route('tasks.show', ['id' => $task->id])
         ->with('success', 'Task created successfully!');
 })->name('tasks.store');
+
+Route::put('/tasks/{id}', function ($id, Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'description' => 'required',
+        'long_description' => 'required'
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data['title'];
+    $task->description = $data['description'];
+    $task->long_description = $data['long_description'];
+    $task->save();
+
+    return redirect()->route('tasks.show', ['id' => $task->id])
+        ->with('success', 'Task updated successfully!');
+})->name('tasks.update');
 
 // Route::get('/xxx', function () {
 //     return 'Hello';
