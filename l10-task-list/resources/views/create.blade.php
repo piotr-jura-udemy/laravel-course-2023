abf4a41c2bd8c4c413828cc1e57c5ee8197ea599
Piotr Jura <jura.piotrek@gmail.com>
2023-03-30 18:39:11 +0200

    0014-old-helper

--- l10-task-list/resources/views/create.blade.php
+++ l10-task-list/resources/views/create.blade.php
@@ -18,7 +18,7 @@
       <label for="title">
         Title
       </label>
-      <input text="text" name="title" id="title" />
+      <input text="text" name="title" id="title" value="{{ old('title') }}" />
       @error('title')
         <p class="error-message">{{ $message }}</p>
       @enderror
@@ -26,7 +26,7 @@
 
     <div>
       <label for="description">Description</label>
-      <textarea name="description" id="description" rows="5"></textarea>
+      <textarea name="description" id="description" rows="5">{{ old('description') }}</textarea>
       @error('description')
         <p class="error-message">{{ $message }}</p>
       @enderror
@@ -34,7 +34,7 @@
 
     <div>
       <label for="long_description">Long Description</label>
-      <textarea name="long_description" id="long_description" rows="10"></textarea>
+      <textarea name="long_description" id="long_description" rows="10">{{ old('long_description') }}</textarea>
       @error('long_description')
         <p class="error-message">{{ $message }}</p>
       @enderror
