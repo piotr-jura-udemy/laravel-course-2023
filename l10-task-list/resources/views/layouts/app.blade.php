2572efd24390cfed1c285785c9631bcd37e98283
Piotr Jura <jura.piotrek@gmail.com>
2023-04-03 18:14:27 +0200

    0023-flash-message-alpine

--- l10-task-list/resources/views/layouts/app.blade.php
+++ l10-task-list/resources/views/layouts/app.blade.php
@@ -4,6 +4,7 @@
 <head>
   <title>Laravel 10 Task List App</title>
   <script src="https://cdn.tailwindcss.com"></script>
+  <script src="//unpkg.com/alpinejs" defer></script>
 
   {{-- blade-formatter-disable --}}
   <style type="text/tailwindcss">
@@ -35,9 +36,22 @@
 
 <body class="container mx-auto mt-10 mb-10 max-w-lg">
   <h1 class="mb-4 text-2xl">@yield('title')</h1>
-  <div>
+  <div x-data="{ flash: true }">
     @if (session()->has('success'))
-      <div>{{ session('success') }}</div>
+      <div x-show="flash"
+        class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+        role="alert">
+        <strong class="font-bold">Success!</strong>
+        <div>{{ session('success') }}</div>
+
+        <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+            stroke-width="1.5" @click="flash = false"
+            stroke="currentColor" class="h-6 w-6 cursor-pointer">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+          </svg>
+        </span>
+      </div>
     @endif
 
     @yield('content')
