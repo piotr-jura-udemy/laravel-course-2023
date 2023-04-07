c25ed3c76a472cae98107d2ff7ca351fdf0634e5
Piotr Jura <jura.piotrek@gmail.com>
2023-04-07 18:55:52 +0200

    0044-local-query-scopes

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Builder;
 
 class Book extends Model
 {
@@ -12,5 +13,10 @@ class Book extends Model
     public function reviews()
     {
         return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title): Builder
+    {
+        return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 }
