331727f07cab14ec457e5853dbadd3a3160ec49a
Piotr Jura <jura.piotrek@gmail.com>
2023-04-10 15:29:43 +0200

    0047-books-with-recent-reviews-FIXED

--- book-review/app/Models/Book.php
+++ book-review/app/Models/Book.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Book extends Model
 {
@@ -20,33 +21,35 @@ class Book extends Model
         return $query->where('title', 'LIKE', '%' . $title . '%');
     }
 
-    public function scopePopular(Builder $query): Builder
+    public function scopePopular(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withCount('reviews')
+        return $query->withCount([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
             ->orderBy('reviews_count', 'desc');
     }
 
-    public function scopeHighestRated(Builder $query): Builder
+    public function scopeHighestRated(Builder $query, $from = null, $to = null): Builder|QueryBuilder
     {
-        return $query->withAvg('reviews', 'rating')
+        return $query->withAvg([
+            'reviews' => fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ], 'rating')
             ->orderBy('reviews_avg_rating', 'desc');
     }
 
-    public function scopeWithRecentReviews(Builder $query, \Closure $interval): Builder
+    public function scopeMinReviews(Builder $query, int $minReviews): Builder|QueryBuilder
     {
-        return $query->whereHas(
-            'reviews',
-            function (Builder $q) use ($interval) {
-                $q->whereBetween(
-                    'created_at',
-                    [$interval(now()), now()]
-                );
-            }
-        );
+        return $query->having('reviews_count', '>=', $minReviews);
     }
 
-    public function scopeWithLastWeekReviews(Builder $query): Builder
+    private function dateRangeFilter(Builder $query, $from = null, $to = null)
     {
-        return $query->withRecentReviews(fn($date) => $date->subWeek());
+        if ($from && !$to) {
+            $query->where('created_at', '>=', $from);
+        } elseif (!$from && $to) {
+            $query->where('created_at', '<=', $to);
+        } elseif ($from && $to) {
+            $query->whereBetween('created_at', [$from, $to]);
+        }
     }
 }
