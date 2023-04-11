c7df18cea47f700e2e27a4a50b83fbf03f246253
Piotr Jura <jura.piotrek@gmail.com>
2023-04-11 19:18:44 +0200

    0049-book-list

--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -5,5 +5,34 @@
 
   <form></form>
 
-  <ul></ul>
+  <ul>
+    @forelse ($books as $book)
+      <li class="mb-4">
+        <div class="book-item">
+          <div
+            class="flex flex-wrap items-center justify-between">
+            <div class="w-full flex-grow sm:w-auto">
+              <a href="{{ route('books.show', $book) }}" class="book-title">{{ $book->title }}</a>
+              <span class="book-author">by {{ $book->author }}</span>
+            </div>
+            <div>
+              <div class="book-rating">
+                {{ number_format($book->reviews_avg_rating, 1) }}
+              </div>
+              <div class="book-review-count">
+                out of {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    @empty
+      <li class="mb-4">
+        <div class="empty-book-item">
+          <p class="empty-text">No books found</p>
+          <a href="{{ route('books.index') }}" class="reset-link">Reset criteria</a>
+        </div>
+      </li>
+    @endforelse
+  </ul>
 @endsection
