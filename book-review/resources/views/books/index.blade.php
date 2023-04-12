02c190f75be4854ea6f3527c45dbefafd20cd7cf
Piotr Jura <jura.piotrek@gmail.com>
2023-04-12 18:59:15 +0200

    0051-book-view-tabs

--- book-review/resources/views/books/index.blade.php
+++ book-review/resources/views/books/index.blade.php
@@ -6,9 +6,29 @@
   <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
     <input type="text" name="title" placeholder="Search by title"
       value="{{ request('title') }}" class="input h-10" />
+    <input type="hidden" name="filter" value="{{ request('filter') }}" />
     <button type="submit" class="btn h-10">Search</button>
     <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
   </form>
+
+  <div class="filter-container mb-4 flex">
+    @php
+      $filters = [
+          '' => 'Latest',
+          'popular_last_month' => 'Popular Last Month',
+          'popular_last_6months' => 'Popular Last 6 Months',
+          'highest_rated_last_month' => 'Highest Rated Last Month',
+          'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+      ];
+    @endphp
+
+    @foreach ($filters as $key => $label)
+      <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+        class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
+        {{ $label }}
+      </a>
+    @endforeach
+  </div>
 
   <ul>
     @forelse ($books as $book)
