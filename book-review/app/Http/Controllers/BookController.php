0aa3d185c30009707021d05f195d164cd57b31f6
Piotr Jura <jura.piotrek@gmail.com>
2023-04-13 17:58:47 +0200

    0053-book-page

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -50,9 +50,16 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Book $book)
     {
-        //
+        return view(
+            'books.show',
+            [
+                'book' => $book->load([
+                    'reviews' => fn($query) => $query->latest()
+                ])
+            ]
+        );
     }
 
     /**
---
+++ book-review/resources/views/books/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="mb-4">
+    <h1 class="mb-2 text-2xl">{{ $book->title }}</h1>
+
+    <div class="book-info">
+      <div class="book-author mb-4 text-lg font-semibold">by {{ $book->author }}</div>
+      <div class="book-rating flex items-center">
+        <div class="mr-2 text-sm font-medium text-slate-700">
+          {{ number_format($book->reviews_avg_rating, 1) }}
+        </div>
+        <span class="book-review-count text-sm text-gray-500">
+          {{ $book->reviews_count }} {{ Str::plural('review', $book->reviews_count) }}
+        </span>
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <h2 class="mb-4 text-xl font-semibold">Reviews</h2>
+    <ul>
+      @forelse ($book->reviews as $review)
+        <li class="book-item mb-4">
+          <div>
+            <div class="mb-2 flex items-center justify-between">
+              <div class="font-semibold">{{ $review->rating }}</div>
+              <div class="book-review-count">
+                {{ $review->created_at->format('M j, Y') }}</div>
+            </div>
+            <p class="text-gray-700">{{ $review->review }}</p>
+          </div>
+        </li>
+      @empty
+        <li class="mb-4">
+          <div class="empty-book-item">
+            <p class="empty-text text-lg font-semibold">No reviews yet</p>
+          </div>
+        </li>
+      @endforelse
+    </ul>
+  </div>
+@endsection
