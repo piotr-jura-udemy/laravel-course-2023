1b47de6e53c26cc816e77c12a2cb9ad0aaef9173
Piotr Jura <jura.piotrek@gmail.com>
2023-04-15 16:44:14 +0200

    0059-rate-limiting

--- book-review/app/Http/Controllers/ReviewController.php
+++ book-review/app/Http/Controllers/ReviewController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class ReviewController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('throttle:reviews')->only(['store']);
+    }
+
     /**
      * Display a listing of the resource.
      */
--- book-review/app/Providers/RouteServiceProvider.php
+++ book-review/app/Providers/RouteServiceProvider.php
@@ -41,8 +41,12 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function configureRateLimiting(): void
     {
-        RateLimiter::for('api', function (Request $request) {
+        RateLimiter::for ('api', function (Request $request) {
             return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+        });
+
+        RateLimiter::for ('reviews', function (Request $request) {
+            return Limit::perHour(3)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
