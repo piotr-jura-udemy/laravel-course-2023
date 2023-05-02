51ff62ec92f95500181c98c5ccc64560736745c7
Piotr Jura <jura.piotrek@gmail.com>
2023-05-02 19:11:56 +0200

    0083-revoking-tokens

--- event-management/app/Http/Controllers/Api/AuthController.php
+++ event-management/app/Http/Controllers/Api/AuthController.php
@@ -39,6 +39,10 @@ class AuthController extends Controller
 
     public function logout(Request $request)
     {
+        $request->user()->tokens()->delete();
 
+        return response()->json([
+            'message' => 'Logged out successfully'
+        ]);
     }
 }
--- event-management/routes/api.php
+++ event-management/routes/api.php
@@ -22,6 +22,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
 
 Route::apiResource('events', EventController::class);
 Route::apiResource('events.attendees', AttendeeController::class)
