0565ed72bd0c532f02335f7bd68dab6b58da7571
Piotr Jura <jura.piotrek@gmail.com>
2023-05-04 19:50:00 +0200

    0088-task-scheduling

--- event-management/app/Console/Kernel.php
+++ event-management/app/Console/Kernel.php
@@ -12,7 +12,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('app:send-event-reminders')
+            ->daily();
     }
 
     /**
@@ -20,7 +21,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands(): void
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
