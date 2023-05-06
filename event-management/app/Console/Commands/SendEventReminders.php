2bce736ee970323681735293a7b8e133e4dc795b
Piotr Jura <jura.piotrek@gmail.com>
2023-05-06 14:22:31 +0200

    0089-notifications-emails

--- event-management/app/Console/Commands/SendEventReminders.php
+++ event-management/app/Console/Commands/SendEventReminders.php
@@ -2,6 +2,7 @@
 
 namespace App\Console\Commands;
 
+use App\Notifications\EventReminderNotification;
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
 
@@ -37,7 +38,11 @@ class SendEventReminders extends Command
 
         $events->each(
             fn($event) => $event->attendees->each(
-                fn($attendee) => $this->info("Notifying the user {$attendee->user->id}")
+                fn($attendee) => $attendee->user->notify(
+                    new EventReminderNotification(
+                        $event
+                    )
+                )
             )
         );
 
---
+++ event-management/app/Notifications/EventReminderNotification.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Notifications;
+
+use App\Models\Event;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class EventReminderNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(
+        public Event $event
+    ) {
+
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->line('Reminder: You have an upcoming event!')
+            ->action('View Event', route('events.show', $this->event->id))
+            ->line(
+                "The event {$this->event->name} starts at {$this->event->start_time}"
+            );
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'event_id' => $this->event->id,
+            'event_name' => $this->event->name,
+            'event_start_time' => $this->event->start_time
+        ];
+    }
+}
