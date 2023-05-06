880c0cf763b5826eba6306caec5c74d6fea2186c
Piotr Jura <jura.piotrek@gmail.com>
2023-05-06 15:05:06 +0200

    0090-queues

--- event-management/app/Notifications/EventReminderNotification.php
+++ event-management/app/Notifications/EventReminderNotification.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class EventReminderNotification extends Notification
+class EventReminderNotification extends Notification implements ShouldQueue
 {
     use Queueable;
 
