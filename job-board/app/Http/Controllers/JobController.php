a80e5e7249ec4cae4236e1c60d01a0f970f79266
Piotr Jura <jura.piotrek@gmail.com>
2023-05-21 18:32:06 +0200

    0130-min-max-salary

--- job-board/app/Http/Controllers/JobController.php
+++ job-board/app/Http/Controllers/JobController.php
@@ -15,8 +15,14 @@ class JobController extends Controller
         $jobs = Job::query();
 
         $jobs->when(request('search'), function ($query) {
-            $query->where('title', 'like', '%' . request('search') . '%')
-                ->orWhere('description', 'like', '%' . request('search') . '%');
+            $query->where(function ($query) {
+                $query->where('title', 'like', '%' . request('search') . '%')
+                    ->orWhere('description', 'like', '%' . request('search') . '%');
+            });
+        })->when(request('min_salary'), function ($query) {
+            $query->where('salary', '>=', request('min_salary'));
+        })->when(request('max_salary'), function ($query) {
+            $query->where('salary', '<=', request('max_salary'));
         });
 
         return view('job.index', ['jobs' => $jobs->get()]);
--- job-board/resources/views/job/index.blade.php
+++ job-board/resources/views/job/index.blade.php
@@ -7,14 +7,15 @@
       <div class="mb-4 grid grid-cols-2 gap-4">
         <div>
           <div class="mb-1 font-semibold">Search</div>
-          <x-text-input name="search" value="" placeholder="Search for any text" />
+          <x-text-input name="search" value="{{ request('search') }}"
+            placeholder="Search for any text" />
         </div>
         <div>
           <div class="mb-1 font-semibold">Salary</div>
 
           <div class="flex space-x-2">
-            <x-text-input name="min_salary" value="" placeholder="From" />
-            <x-text-input name="max_salary" value="" placeholder="To" />
+            <x-text-input name="min_salary" value="{{ request('min_salary') }}" placeholder="From" />
+            <x-text-input name="max_salary" value="{{ request('max_salary') }}" placeholder="To" />
           </div>
         </div>
         <div>3</div>
