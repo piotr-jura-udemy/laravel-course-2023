40e41f6f87bc16b648374b93ff47718fdc84fb9a
Piotr Jura <jura.piotrek@gmail.com>
2023-06-19 17:12:19 +0200

    0155-actual-file-upload

--- job-board/app/Http/Controllers/JobApplicationController.php
+++ job-board/app/Http/Controllers/JobApplicationController.php
@@ -16,11 +16,19 @@ class JobApplicationController extends Controller
     public function store(Job $job, Request $request)
     {
         $this->authorize('apply', $job);
+
+        $validatedData = $request->validate([
+            'expected_salary' => 'required|min:1|max:1000000',
+            'cv' => 'required|file|mimes:pdf|max:2048'
+        ]);
+
+        $file = $request->file('cv');
+        $path = $file->store('cvs', 'private');
+
         $job->jobApplications()->create([
             'user_id' => $request->user()->id,
-            ...$request->validate([
-                'expected_salary' => 'required|min:1|max:1000000'
-            ])
+            'expected_salary' => $validatedData['expected_salary'],
+            'cv_path' => $path
         ]);
 
         return redirect()->route('jobs.show', $job)
--- job-board/resources/views/job_application/create.blade.php
+++ job-board/resources/views/job_application/create.blade.php
@@ -13,12 +13,18 @@
       Your Job Application
     </h2>
 
-    <form action="{{ route('job.application.store', $job) }}" method="POST">
+    <form action="{{ route('job.application.store', $job) }}" method="POST"
+      enctype="multipart/form-data">
       @csrf
       <div class="mb-4">
         <label for="expected_salary"
           class="mb-2 block text-sm font-medium text-slate-900">Expected Salary</label>
         <x-text-input type="number" name="expected_salary" />
+      </div>
+
+      <div class="mb-4">
+        <label class="mb-2 block text-sm font-medium text-slate-900">Upload CV</label>
+        <x-text-input type="file" name="cv" />
       </div>
 
       <x-button class="w-full">Apply</x-button>
