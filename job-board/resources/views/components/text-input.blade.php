a3a95f335e8daa9cd9f802c0156a8fa95b5d0fdf
Piotr Jura <jura.piotrek@gmail.com>
2023-06-21 18:19:58 +0200

    0156-refactor-error-display

--- job-board/resources/views/components/text-input.blade.php
+++ job-board/resources/views/components/text-input.blade.php
@@ -1,15 +1,35 @@
 <div class="relative">
-  @if ($formRef)
-    <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
-      @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-        stroke="currentColor" class="h-4 w-4 text-slate-500">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-      </svg>
-    </button>
+  @if ('textarea' != $type)
+    @if ($formRef)
+      <button type="button" class="absolute top-0 right-0 flex h-full items-center pr-2"
+        @click="$refs['input-{{ $name }}'].value = ''; $refs['{{ $formRef }}'].submit();">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+          stroke="currentColor" class="h-4 w-4 text-slate-500">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+        </svg>
+      </button>
+    @endif
+    <input x-ref="input-{{ $name }}" type="{{ $type }}"
+      placeholder="{{ $placeholder }}"
+      name="{{ $name }}" value="{{ old($name, $value) }}" id="{{ $name }}"
+      @class([
+          'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+          'pr-8' => $formRef,
+          'ring-slate-300' => !$errors->has($name),
+          'ring-red-300' => $errors->has($name),
+      ]) />
+  @else
+    <textarea id="{{ $name }}" name="{{ $name }}" @class([
+        'w-full rounded-md border-0 py-1.5 px-2.5 text-sm ring-1 placeholder:text-slate-400 focus:ring-2',
+        'pr-8' => $formRef,
+        'ring-slate-300' => !$errors->has($name),
+        'ring-red-300' => $errors->has($name),
+    ])>{{ old($name, $value) }}</textarea>
   @endif
-  <input x-ref="input-{{ $name }}" type="{{ $type }}"
-    placeholder="{{ $placeholder }}"
-    name="{{ $name }}" value="{{ $value }}" id="{{ $name }}"
-    class="w-full rounded-md border-0 py-1.5 px-2.5 pr-8 text-sm ring-1 ring-slate-300 placeholder:text-slate-400 focus:ring-2" />
+
+  @error($name)
+    <div class="mt-1 text-xs text-red-500">
+      {{ $message }}
+    </div>
+  @enderror
 </div>
