808956ef7eea99d05428e4b37f124f27ab0cc345
Piotr Jura <jura.piotrek@gmail.com>
2023-06-22 18:11:14 +0200

    0158-flash-errors

--- job-board/resources/views/components/layout.blade.php
+++ job-board/resources/views/components/layout.blade.php
@@ -47,6 +47,13 @@
       <p>{{ session('success') }}</p>
     </div>
   @endif
+  @if (session('error'))
+    <div role="alert"
+      class="my-8 rounded-md border-l-4 border-red-300 bg-red-100 p-4 text-red-700 opacity-75">
+      <p class="font-bold">Error!</p>
+      <p>{{ session('error') }}</p>
+    </div>
+  @endif
 
   {{ $slot }}
 </body>
